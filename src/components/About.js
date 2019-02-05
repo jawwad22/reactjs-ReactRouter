fbe663d9d7845a37ac95d5ec11ad6f8ee55bd194
unknown <mjawwadiqbal22@gmail.com>
2019-02-04 23:26:36 -0800

    Component Added

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About=()=>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi sapiente dolorum fugit facilis consequuntur explicabo. Quibusdam rem architecto natus nulla, ea incidunt hic nam et optio corrupti iste quod modi!</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact=()=>{
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi sapiente dolorum fugit facilis consequuntur explicabo. Quibusdam rem architecto natus nulla, ea incidunt hic nam et optio corrupti iste quod modi!</p>
+        </div>
+    )
+}
+
+export default Contact
