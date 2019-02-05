faeaa6a0496aa5aa127d6e7dc584175604d2001d
unknown <mjawwadiqbal22@gmail.com>
2019-02-05 01:49:05 -0800

    --Wrapped COmponent

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import {Link,NavLink} from 'react-router-dom'
+import {Link,NavLink,withRouter} from 'react-router-dom'
 
-const Navbar=()=>{
-
+const Navbar=(props)=>{
+ setTimeout(()=>{
+     props.history.push('/about')
+ },2000)
     return(
         <nav className="nav-wrapper red darkern-3">
             <div className="container">
@@ -20,4 +22,4 @@ const Navbar=()=>{
     )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
