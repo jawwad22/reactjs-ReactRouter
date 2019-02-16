891fecd007b7489ce9375145b643acf995b791cf
unknown <mjawwadiqbal22@gmail.com>
2019-02-16 11:47:01 -0800

    --appying redux

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,22 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 
 class Home extends Component {
-    state = {
-        posts: []
-    }
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                this.setState({
-                    posts: res.data.slice(0, 10)
-                })
-            })
-
-    }
 
     render() {
-        const { posts } = this.state;
+        console.log(this.props)
+        const { posts } = this.props;
         const postList = posts.length ? (
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
                         <div className="card-content">
-                            <Link to={'/'+post.id}>
-                            <span className="card-title">
-                                {post.title}
-                            </span>
+                            <Link to={'/' + post.id}>
+                                <span className="card-title">
+                                    {post.title}
+                                </span>
                             </Link>
                             <p>{post.body}</p>
                         </div>
@@ -41,9 +31,15 @@ class Home extends Component {
         return (
             <div className="container">
                 <h4 className="center">Home</h4>
-{postList}
+                {postList}
             </div>
         )
     }
 }
-export default Home
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+export default connect(mapStateToProps)(Home)
